61c3df015b507e1d810251f464088eb18c65150e
Jackie Casper <casper.jaclyn@gmail.com>
2018-12-10 11:39:31 +0300

    starter app

---
+++ controllers/pokemonController.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var pokemon = require('../models/pokemon');
+
+router.get('/', pokemon.getAll, renderIndex);
+router.get('/:id', pokemon.find, renderShow)
+
+function renderIndex(req, res){
+  mustacheVariables = {
+    pokemon: res.locals.pokemon
+  }
+  console.log(mustacheVariables)
+  res.render('./pokemon/index', mustacheVariables);
+}
+
+function renderShow(req,res){
+  mustacheVariables = res.locals.pokemon;
+  res.render('./pokemon/show', mustacheVariables);
+}
+
+module.exports = router;
